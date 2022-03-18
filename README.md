07a55c6b968a8fd9b0fc5d25731f89753afc9292
Angel David Diaz Muñoz <arcangelzith@gmail.com>
2022-03-18 16:51:39 -0500

    Add: Install Oh-My-Posh and Zsh on Ubuntu

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# setup
+# Setup
+
 Settings and customization scripts
+
+
+```bash
+wget https://raw.githubusercontent.com/arcangelzith/my-setup/main/ubuntu/install.sh | bash
+```
---
+++ ubuntu/install.sh
@@ -0,0 +1,16 @@
+$myrepo=https://raw.githubusercontent.com/arcangelzith/my-setup/main/
+
+sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
+sudo chmod +x /usr/local/bin/oh-my-posh
+
+sudo mkdir /usr/local/etc/oh-my-posh/
+
+sudo wget "${myrepo}/Oh-My-Posh/arcangel-ps.omp.json" -O /usr/local/etc/oh-my-posh/arcangel-ps.omp.json
+sudo wget "${myrepo}/Oh-My-Posh/arcangel-sh.omp.json" -O /usr/local/etc/oh-my-posh/arcangel-sh.omp.json
+
+sudo apt update
+sudo apt install zsh
+sudo apt autoremove
+sudo apt autoclean
+
+sudo wget "${myrepo}/ubuntu/.arcangelrc" -O ~/.arcangelrc
