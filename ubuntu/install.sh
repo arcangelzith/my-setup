f8ef80cad0949b284285e8e965332e71d625b4e7
Angel David Diaz Muñoz <arcangelzith@gmail.com>
2022-03-19 15:32:05 -0500

    Update: Allow local user in Ubuntu installer

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -5,14 +5,18 @@ myrepo=https://raw.githubusercontent.com/arcangelzith/my-setup/main/
 shellName="$(sh -c 'ps -p $$ -o ppid=' | xargs ps -o comm= -p)"
 ompTheme="arcan-sh.omp.json"
 
+userName="$(whoami)"
+userRc=".${userName}rc"
+userRcFile="~/$userName"
+
 if ! [[ -f /usr/local/bin/oh-my-posh ]]; then
     sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
     sudo chmod +x /usr/local/bin/oh-my-posh
 
     sudo mkdir /usr/local/etc/oh-my-posh/
 
-    sudo wget "${myrepo}/Oh-My-Posh/arcan-ps.omp.json" -O /usr/local/etc/oh-my-posh/arcan-ps.omp.json
-    sudo wget "${myrepo}/Oh-My-Posh/arcan-sh.omp.json" -O /usr/local/etc/oh-my-posh/arcan-sh.omp.json
+    sudo wget "$myrepo/Oh-My-Posh/arcan-ps.omp.json" -O /usr/local/etc/oh-my-posh/arcan-ps.omp.json
+    sudo wget "$myrepo/Oh-My-Posh/arcan-sh.omp.json" -O /usr/local/etc/oh-my-posh/arcan-sh.omp.json
 fi
 
 if ! [[ -f /usr/bin/zsh ]]; then
@@ -22,27 +26,27 @@ if ! [[ -f /usr/bin/zsh ]]; then
     sudo apt autoclean
 fi
 
-if ! [[ -f ~/.arcanrc ]]; then
-    wget "${myrepo}/ubuntu/.arcanrc" -O ~/.arcanrc
+if ! [[ -f $userRcFile ]]; then
+    wget "$myrepo/ubuntu/.arcanrc" -O $userRcFile
 fi
 
 arcanrc="
-if [ -f ~/.arcanrc ]; then
-    . ~/.arcanrc
+if [ -f $userRcFile ]; then
+    . $userRcFile
 fi
 "
 
-echo "Check if ~/.arcanrc is registered in ~/.bashrc"
+echo "Check if $userRcFile is registered in ~/.bashrc"
 
-if ! grep '.arcanrc' ~/.bashrc; then
-    echo "Adding ~/.arcanrc to ~/.bashrc"
+if ! grep "$userRc" ~/.bashrc; then
+    echo "Adding $userRcFile to ~/.bashrc"
     echo "$arcanrc" >> ~/.bashrc
 fi
 
-echo "Check if ~/.arcanrc is registered in ~/.zshrc"
+echo "Check if $userRcFile is registered in ~/.zshrc"
 
-if ! grep '.arcanrc' ~/.zshrc; then
-    echo "Adding ~/.arcanrc to ~/.zshrc"
+if ! grep "$userRc" ~/.zshrc; then
+    echo "Adding $userRcFile to ~/.zshrc"
     echo "$arcanrc" >> ~/.zshrc
 fi
 
