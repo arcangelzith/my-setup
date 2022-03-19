5f3bbd53bca37d38a7c5a26e86484ec69919bf57
Angel David Diaz Muñoz <arcangelzith@gmail.com>
2022-03-19 12:15:56 -0500

    Fix: Ubuntu installer

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -2,17 +2,36 @@
 
 myrepo=https://raw.githubusercontent.com/arcangelzith/my-setup/main/
 
+shellName="$(sh -c 'ps -p $$ -o ppid=' | xargs ps -o comm= -p)"
+ompTheme="arcan-sh.omp.json"
+
 sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
 sudo chmod +x /usr/local/bin/oh-my-posh
 
 sudo mkdir /usr/local/etc/oh-my-posh/
 
-sudo wget "${myrepo}/Oh-My-Posh/arcangel-ps.omp.json" -O /usr/local/etc/oh-my-posh/arcangel-ps.omp.json
-sudo wget "${myrepo}/Oh-My-Posh/arcangel-sh.omp.json" -O /usr/local/etc/oh-my-posh/arcangel-sh.omp.json
+sudo wget "${myrepo}/Oh-My-Posh/arcan-ps.omp.json" -O /usr/local/etc/oh-my-posh/arcan-ps.omp.json
+sudo wget "${myrepo}/Oh-My-Posh/arcan-sh.omp.json" -O /usr/local/etc/oh-my-posh/arcan-sh.omp.json
 
 sudo apt update
-sudo apt install zsh
+sudo apt install zsh −−yes
 sudo apt autoremove
 sudo apt autoclean
 
-sudo wget "${myrepo}/ubuntu/.arcangelrc" -O ~/.arcangelrc
+wget "${myrepo}/ubuntu/.arcanrc" -O ~/.arcanrc
+
+omprc="eval '\$(oh-my-posh --init --shell $shellName --config /usr/local/etc/oh-my-posh/$ompTheme'" > 
+
+arcanrc="
+if [ -f ~/.arcanrc ]; then
+    . ~/.arcanrc
+fi
+"
+
+if [ grep ".arcanrc" ~/.bashrc ]; then
+    echo $arcanrc >> .bashrc
+fi
+
+if [ grep ".arcanrc" ~/.zshrc ]; then
+    echo $arcanrc >> .zshrc
+fi
