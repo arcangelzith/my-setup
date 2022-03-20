26551148500cb39ef3dd974ff046ff6d8ea0de0a
Angel David Diaz Muñoz <arcangelzith@gmail.com>
2022-03-19 19:26:53 -0500

    Update: Ubuntu installer

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
-myrepo=https://raw.githubusercontent.com/arcangelzith/my-setup/main/
+myrepo=https://raw.githubusercontent.com/arcangelzith/my-setup/main
 
 shellName="$(sh -c 'ps -p $$ -o ppid=' | xargs ps -o comm= -p)"
 ompTheme="arcan-sh.omp.json"
 
 userName="$(whoami)"
 userRc=".${userName}rc"
-userRcFile="~/$userRc"
+userRcFile="$HOME/$userRc"
 
 if ! [[ -f /usr/local/bin/oh-my-posh ]]; then
+    echo "Preparing Oh-My-Posh installation"
+
     sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
     sudo chmod +x /usr/local/bin/oh-my-posh
 
@@ -20,13 +22,19 @@ if ! [[ -f /usr/local/bin/oh-my-posh ]]; then
 fi
 
 if ! [[ -f /usr/bin/zsh ]]; then
+    echo "Preparing zsh installation"
+
     sudo apt update
     sudo apt install zsh −−yes
     sudo apt autoremove
     sudo apt autoclean
 fi
 
-if ! [[ -f "$userRcFile" ]]; then
+echo "Check if $userRcFile exists"
+
+if ! [[ -f $userRcFile ]]; then
+    echo "Downloading $userRcFile from $myrepo/ubuntu/$userRc"
+
     wget "$myrepo/ubuntu/$userRc" -O $userRcFile
 fi
 
@@ -54,7 +62,7 @@ echo "Check if Oh-My-Posh is registered in ~/.bashrc"
 
 if ! grep 'oh-my-posh' ~/.bashrc; then
     echo "Adding oh-my-posh to ~/.bashrc"
-#    echo "eval '\$(oh-my-posh --init --shell bash --config /usr/local/etc/oh-my-posh/$ompTheme)'" >> ~/.bashrc
+#   echo "eval '\$(oh-my-posh --init --shell bash --config /usr/local/etc/oh-my-posh/$ompTheme)'" >> ~/.bashrc
 fi
 
 echo "Check if Oh-My-Posh is registered in ~/.zshrc"
@@ -63,4 +71,3 @@ if ! grep 'oh-my-posh' ~/.zshrc; then
     echo "Adding oh-my-posh to ~/.zshrc"
     echo "eval '\$(oh-my-posh --init --shell zsh --config /usr/local/etc/oh-my-posh/$ompTheme)'" >> ~/.zshrc
 fi
- 
