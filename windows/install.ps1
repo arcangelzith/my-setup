5c8d471a5fa1d18a831adf1483378e92ebc1e8f7
Angel David Diaz Muñoz <arcangelzith@users.noreply.github.com>
2023-04-10 16:06:01 -0500

    Fix: Installing PS profile doesn't copy the themes

--- windows/install.ps1
+++ windows/install.ps1
@@ -4,9 +4,10 @@
 
 # Usar el nombre de la carpeta actual del usuario como el nombre del perdil .ps1
 
+$myrepo="https://raw.githubusercontent.com/arcangelzith/my-setup/main"
 $user_pwsh_dir="$(Split-Path $PROFILE)"
 
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/arcangelzith/my-setup/main/windows/arcan.ps1?t=$((Get-Date).Ticks)" -OutFile "$user_pwsh_dir\arcan.ps1"
+Invoke-WebRequest -Uri "$myrepo/windows/arcan.ps1?t=$((Get-Date).Ticks)" -OutFile "$user_pwsh_dir\arcan.ps1"
 
 # Agregar importación a arcan.ps1 solo si no está importado antes
 
@@ -23,5 +24,11 @@ if (Test-Path -Path $PROFILE) {
     echo $userps1Imports > $PROFILE
 }
 
+Import-Module Oh-My-Posh
+
+Invoke-WebRequest -Uri "$myrepo/Oh-My-Posh/arcan-ps.omp.json?t=$(date +%s)" -OutFile "$env:POSH_THEMES_PATH\arcan-ps.omp.json"
+Invoke-WebRequest -Uri "$myrepo/Oh-My-Posh/arcan-sh.omp.json?t=$(date +%s)" -OutFile "$env:POSH_THEMES_PATH\arcan-sh.omp.json"
+
+Remove-Variable myrepo
 Remove-Variable user_pwsh_dir
 Remove-Variable userps1Imports
